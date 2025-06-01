927753dd7880480894d016e90073ab2cc25f06f9
Momchil Marinov <mom4ilakis@gmail.com>
2025-06-01 15:35:16 +0300

    Add relationship properties to bug and user models

--- backend/src/models/bug.py
+++ backend/src/models/bug.py
@@ -1,20 +1,29 @@
-from sqlmodel import Field
+from sqlmodel import Field, Relationship
 from sqlalchemy import Column, Enum as SQLEnum, String
 import uuid as _uuid
 
 from constants import Status, Priority, Severity
 from .timestamp_mixin import TimeStampMixin
+from .user import User
+
 
 class Bug(TimeStampMixin, table=True):
     id: int = Field(primary_key=True)
-    uuid:_uuid.UUID = Field(default_factory=_uuid.uuid4, unique=True, index=True)
+    uuid: _uuid.UUID = Field(default_factory=_uuid.uuid4, unique=True, index=True)
     title: str = Field(max_length=255)
     description: str | None = Field(max_length=1000)
     steps: str | None = Field(nullable=True, max_length=255)
     expected: str | None = Field(nullable=True, max_length=255)
     actual: str | None = Field(nullable=True, max_length=255)
     status: Status = Field(sa_column=Column(SQLEnum(Status), server_default=Status.NEW.value, nullable=False))
-    priority: Priority = Field(sa_column=Column(SQLEnum(Priority), default=Priority.MEDIUM, server_default=Priority.MEDIUM.value, nullable=False))
-    severity: Severity = Field(sa_column=Column(SQLEnum(Severity), default=Severity.MEDIUM, server_default=Severity.MEDIUM.value, nullable=False))
+    priority: Priority = Field(
+        sa_column=Column(SQLEnum(Priority), default=Priority.MEDIUM, server_default=Priority.MEDIUM.value,
+                         nullable=False))
+    severity: Severity = Field(
+        sa_column=Column(SQLEnum(Severity), default=Severity.MEDIUM, server_default=Severity.MEDIUM.value,
+                         nullable=False))
     reported_by: int | None = Field(default=None, foreign_key="user.id")
+    reporter: User = Relationship(back_populates="bugs", sa_relationship_kwargs={"foreign_keys": "Bug.reported_by"})
     assigned_to: int | None = Field(default=None, foreign_key="user.id")
+    assignee: User | None = Relationship(back_populates="assigned_bugs",
+                                         sa_relationship_kwargs={"foreign_keys": "Bug.assigned_to"})
--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,6 +1,6 @@
 import uuid as _uuid
 
-from sqlmodel import Field
+from sqlmodel import Field, Relationship
 
 from .timestamp_mixin import TimeStampMixin
 
@@ -14,3 +14,5 @@ class User(TimeStampMixin, table=True):
     is_active: bool = Field(default=True)
     is_superuser: bool = Field(default=False)
     is_verified: bool = Field(default=False)
+    bugs: list["Bug"] = Relationship(back_populates="reporter", sa_relationship_kwargs={"foreign_keys": "Bug.reported_by"})
+    assigned_bugs: list["Bug"] = Relationship(back_populates="assignee", sa_relationship_kwargs={"foreign_keys": "Bug.assigned_to"})
