851fe2fcedeb2ee55168da812b964dd4a3965164
Momchil Marinov <mom4ilakis@gmail.com>
2025-06-03 01:16:43 +0300

    Charka UI setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {Provider} from "@/components/ui/provider"
 import './index.css'
 import App from './App.tsx'
+import {RouterProvider} from "react-router/dom";
+import {createBrowserRouter} from "react-router";
+import {LoginPage} from "@/components/ui/login.tsx";
+import {RegisterPage} from "@/components/ui/register.tsx";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <Provider>
+                <RouterProvider router={createBrowserRouter([
+                    {
+                        element: <ProtectedRoute/>,
+                        children: [{
+                            path: '/',
+                            element: <App/>
+                        }]
+
+                    }, {
+                        path: '/login',
+                        element: <LoginPage/>,
+                    }, {
+                        path: '/register',
+                        element: <RegisterPage/>,
+                    }
+
+                ])}/>
+        </Provider>
+    </StrictMode>,
 )
