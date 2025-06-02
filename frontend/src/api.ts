0c4a3b463efd1f986ac8ae92d508a997bbd73d57
Momchil Marinov <mom4ilakis@gmail.com>
2025-06-03 01:15:38 +0300

    Create FE API

---
+++ frontend/src/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api/v1/',
+    headers: {"Content-Type": "application/json"}
+});
+
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+api.interceptors.response.use(response => response, async error => {
+    if (error.response.status === 401) {
+        localStorage.removeItem('token');
+    }
+    window.location.reload();
+    return Promise.reject(error);
+});
+
+export default api;
